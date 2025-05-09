c7cbcaa4a358ad664281a6f100f35fc7cdfb8177
Kevin Duffy <soadhlh@gmail.com>
2025-05-08 21:05:53 -0400

    Init commit
    Includes npm packages I believe I need to finish this project

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+const assetsPath = path.join(__dirname, "public");
+
+app.use(express.static(assetsPath));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index", {} );
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Express listening on port ${PORT}`);
+});
