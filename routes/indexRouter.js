e10b0365a0375bb13a273201ecd8ccfec57889e5
Kevin Duffy <soadhlh@gmail.com>
2025-05-09 18:42:21 -0400

    Index displays form to submit messages

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,24 @@
 const { Router } = require("express");
 const indexRouter = Router();
 
-indexRouter.use("/", (req, res) => {
-    res.render("index", {} );
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+indexRouter.get("/", (req, res) => {
+    res.render("index", {
+        title: "mini Messageboard",
+        messages: messages
+    });
 });
 
 module.exports = indexRouter;
