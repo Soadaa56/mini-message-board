ef845d6157ac3be16b7160433e362c67626fa798
Kevin Duffy <soadhlh@gmail.com>
2025-05-16 11:44:23 -0400

    Methods to query db
    Ending this section of TOP sql projects

--- db/queries.js
+++ db/queries.js
@@ -5,6 +5,17 @@ async function getAllUserMessages() {
   return rows;
 }
 
+async function postUserMessage(messageUsername, messageMessage) {
+  await pool.query("INSERT INTO messages (username, message) VALUES ($1, $2)", [messageUsername, messageMessage]);
+}
+
+async function getUserMessage(messageIndex) {
+  const result = await pool.query("Select * FROM messages WHERE id = $1", [messageIndex]);
+  return result.rows[0];
+}
+
 module.exports = {
-  getAllUserMessages
+  getAllUserMessages,
+  postUserMessage,
+  getUserMessage
 }
